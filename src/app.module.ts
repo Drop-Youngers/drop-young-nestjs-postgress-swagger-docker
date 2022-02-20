2eb8e5fe4b686005ff4b84046011990a989aeb02
claranceliberi <liberintwari@gmail.com>
2022-02-20 21:56:47 +0200

    chore: sample module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'liberi',
+      password: 'liberi',
+      database: 'learndb',
+      entities: [],
+      synchronize: true,
+      dropSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
